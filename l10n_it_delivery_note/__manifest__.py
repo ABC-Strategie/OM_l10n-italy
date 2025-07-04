51c96739f7a430de4eea859bd735ca1e8bfeb458
masimassimo <33621723+masimassimo@users.noreply.github.com>
2025-07-04 10:22:31 +0200

    FIX4Mig Update __manifest__.py

--- l10n_it_delivery_note/__manifest__.py
+++ l10n_it_delivery_note/__manifest__.py
@@ -33,7 +33,7 @@
         "security/ir_rule.xml",
         "security/res_groups.xml",
         "security/res_users.xml",
-        "data/delivery_note_data.xml",
+        #"data/delivery_note_data.xml",
         "report/delivery_data.xml",
         "report/report_delivery_note.xml",
         "views/account_move.xml",
