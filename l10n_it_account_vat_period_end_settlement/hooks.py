9e28eb960c28afd58897aa7e15b2362b0522a141
masimassimo <33621723+masimassimo@users.noreply.github.com>
2026-02-16 12:48:40 +0100

    Update hooks.py

--- l10n_it_account_vat_period_end_settlement/hooks.py
+++ l10n_it_account_vat_period_end_settlement/hooks.py
@@ -14,6 +14,10 @@ def set_exclude_from_vat_settlements(env):
 
 
 def pre_absorb_old_module(env):
+    _disable_view_by_xmlid(env, "l10n_it_vat_statement_communication.view_tax_vsc_form")
+    _disable_view_by_xmlid(env, "studio_customization.odoo_studio_fatturap_37189039-d2c5-4187-b7be-814a1f91510b")
+    _disable_view_by_xmlid(env, "abc_efatturazione.fatturapa_attachment_in_tree_date")
+    _disable_view_by_xmlid(env, "l10n_it_fatturapa_export_zip.view_fatturapa_in_attachment_tree")
     if openupgrade.is_module_installed(env.cr, "account_vat_period_end_statement"):
         openupgrade.update_module_names(
             env.cr,
@@ -26,10 +30,7 @@ def pre_absorb_old_module(env):
             merge_modules=True,
         )
         #set_exclude_from_vat_settlements(env)
-        _disable_view_by_xmlid(env, "l10n_it_vat_statement_communication.view_tax_vsc_form")
-        _disable_view_by_xmlid(env, "studio_customization.odoo_studio_fatturap_37189039-d2c5-4187-b7be-814a1f91510b")
-        _disable_view_by_xmlid(env, "abc_efatturazione.fatturapa_attachment_in_tree_date")
-        _disable_view_by_xmlid(env, "l10n_it_fatturapa_export_zip.view_fatturapa_in_attachment_tree")
+        
 
 
 def _disable_view_by_xmlid(env, xmlid: str):
