82247f399cb13bc9df598185186ebb75b1dfb12a
masimassimo <dr.masi.massimo@gmail.com>
2026-04-02 12:54:34 +0200

    Update post-migration.py

--- l10n_it_account_stamp/migrations/19.0.1.0.0/post-migration.py
+++ l10n_it_account_stamp/migrations/19.0.1.0.0/post-migration.py
@@ -2,10 +2,12 @@
 #  License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from openupgradelib import openupgrade
+from odoo import api, SUPERUSER_ID
 
 def migrate(cr, version):
     # Aggiorna i record bloccati ricaricando l'XML da data.xml (bypassando noupdate)
     cr.execute("UPDATE ir_model_data SET noupdate=False WHERE module='l10n_it_account_stamp' AND name='l10n_it_account_stamp_2_euro'")
     cr.execute("UPDATE ir_model_data SET noupdate=False WHERE module='base' AND name='main_company'")
     
-    openupgrade.load_data(cr, 'l10n_it_account_stamp', 'data/data.xml')
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    openupgrade.load_data(env, 'l10n_it_account_stamp', 'data/data.xml')
